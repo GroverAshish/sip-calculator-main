f0c39ca32e987274caa1f4f683462dfe3a82ad7f
GroverAshish <91431919+GroverAshish@users.noreply.github.com>
2022-02-15 22:07:09 +0530

    Update index.js

--- Routes/index.js
+++ Routes/index.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const srv = require("../controllers/index")
 
 // SIP Calculation
-router.post("/SIP", srv.sip)
+router.post("/sipCalculator", srv.sip)
 
 module.exports = router
